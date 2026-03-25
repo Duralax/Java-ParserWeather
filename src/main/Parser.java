1641c90d5419f515404f312a7978fe1b76632158
Duralax <73173215+Duralax@users.noreply.github.com>
2026-03-25 20:37:01 +0500

    Initial commit

---
+++ src/main/Parser.java
@@ -0,0 +1,9 @@
+package main;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Parser {
+    private static final String URL = "https://www.gismeteo.ru/weather-yekaterinburg-4517/";
+
+}
